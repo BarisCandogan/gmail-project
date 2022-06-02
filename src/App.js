52f58ee953ce2f04d73c53acc9257a415ae796a1
Brs545 <baris.candogan@hotmail.com>
2022-06-02 23:25:44 +0300

    auth user logged in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 import Header from './Header'
 import Sidebar from './Sidebar'
@@ -6,20 +6,50 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Mail from './Mail'
 import EmailList from './EmailList'
 import SendMail from './SendMail'
+import { useDispatch, useSelector } from 'react-redux'
+import { SendMessageIsOpen } from './features/mailSlice'
+import { selectUser } from './features/UserSlice'
+import Login from './Login'
+import { auth } from './firebase'
+import { login } from './features/UserSlice'
+
 function App() {
+  const sendMessageIsOpen = useSelector(SendMessageIsOpen)
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(
+          login({
+            displayname: user.displayName,
+            email: user.email,
+            photoUrl: user.photoURL,
+          })
+        )
+      } else {
+      }
+    })
+  }, [])
+
   return (
     <BrowserRouter>
-      <div className='app'>
-        <Header />
-        <div className='app_body'>
-          <Sidebar />
-          <Routes>
-            <Route path='/Mail' element={<Mail />} />
-            <Route path='/' element={<EmailList />} />
-          </Routes>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className='app'>
+          <Header />
+          <div className='app_body'>
+            <Sidebar />
+            <Routes>
+              <Route path='/Mail' element={<Mail />} />
+              <Route path='/' element={<EmailList />} />
+            </Routes>
+          </div>
+          {sendMessageIsOpen && <SendMail />}
         </div>
-        <SendMail />
-      </div>
+      )}
     </BrowserRouter>
   )
 }
